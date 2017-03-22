bd92f726f14143d94e3a12f99266784983286461
Yura Pasternak <yurapaster@gmail.com>
2017-03-22 22:06:46 +0200

    classes for category views

---
+++ app/src/main/java/id_iot/demo/CategoryAdapter.java
@@ -0,0 +1,59 @@
+package id_iot.demo;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    private ArrayList<Recipe> recipes;
+
+    public CategoryAdapter(ArrayList<Recipe> recipes){
+        this.recipes = recipes;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ImageView itemImage;
+        public TextView itemName;
+        private CardView cardView;
+
+        public ViewHolder (View view){
+            super(view);
+            itemImage = (ImageView) itemView.findViewById(R.id.category_image);
+            itemName = (TextView) itemView.findViewById(R.id.category_name);
+            cardView = (CardView) itemView.findViewById(R.id.cardview_category);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.card_view_category, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, int i){
+        viewHolder.itemName.setText(recipes.get(i).getRecipeName());
+        viewHolder.itemImage.setImageResource(recipes.get(i).getRecipePhoto());
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), recipes.get(viewHolder.getAdapterPosition()).getRecipeName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount(){
+        return recipes.size();
+    }
+}
---
+++ app/src/main/java/id_iot/demo/CategoryFragment.java
@@ -0,0 +1,32 @@
+package id_iot.demo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CategoryFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.recycler_view_category, container, false);
+        getActivity().setTitle("Menu");
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+
+        layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        final CategoryAdapter adapter = new CategoryAdapter(((MainActivity) getActivity()).getRecipesList());
+        recyclerView.setAdapter(adapter);
+        return view;
+    }
+}
